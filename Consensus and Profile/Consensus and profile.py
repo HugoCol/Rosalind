8fa543acd72c00e185d61f1dfdf87c61518a06ed
HugoCol <hugo.colenbrander@gmail.com>
2020-08-08 11:50:14 +0200

    Consensus and profile alsmost done

--- Consensus and Profile/Consensus and profile.py
+++ Consensus and Profile/Consensus and profile.py
@@ -11,14 +11,16 @@ def matrix_builder(file):
     sequence_dictionary = {}
 
     for line in file:
-        if line.startswith('>'):
-            k=line.replace('\n','')
+        if line.startswith(">"):
+            k = line.replace("\n", "")
             sequence_dictionary[k] = ""
         else:
-            sequence_dictionary[k] = sequence_dictionary[k] + line.replace("\n","")
+            sequence_dictionary[k] = sequence_dictionary[k] + line.replace(
+                "\n", "")
             # sequence_dictionary.append(line)
     print(sequence_dictionary)
     return sequence_dictionary
+
 
 def scoring(sequence_dictionary):
     """count the amount of amino acid in each colomn of the matrix,
@@ -31,16 +33,18 @@ def scoring(sequence_dictionary):
     g_list = 'G:'
     dict_hold = []
     consensuspattern = ''
+
     # take the length of the first sequence, which should be equal to all
     # other in a matrix.
-    """Still have to get the proper length of the first value in the 
-    dictionary"""
-    length = range(len(sequence_dictionary))
-    print(length)
+    # getting the first value from a dictionary to determine the length,
+    # There probably is a better way to do this
+    first_value = [elem for elem in sequence_dictionary.values()]
+    length = range(len(first_value[0]))
+
     # for every column, make add a dictionary to a list
     for i in length:
-        dict_hold.append({'A':0,'T':0,'C':0,'G':0})
-        # for
+        dict_hold.append({'A': 0, 'T': 0, 'C': 0, 'G': 0})
+
         for n in sequence_dictionary.values():
             m = n[i]
             # i is the location in the list and m is the dictionary key
@@ -56,29 +60,31 @@ def scoring(sequence_dictionary):
         t_list = t_list + " " + str(t)
         c_list = c_list + " " + str(c)
         g_list = g_list + " " + str(g)
-        consensus_char = (max(a,t,c,g))
+        consensus_char = (max(a, t, c, g))
         # The nucleotides are counted in variables a,t,c,g. If that amount
         # matches the highest counted number that nucleotide gets added to
         # the consensus pattern
         if a == consensus_char:
-            consensuspattern= consensuspattern+'A'
-        if t == consensus_char:
-            consensuspattern= consensuspattern+'T'
-        if c == consensus_char:
-            consensuspattern= consensuspattern+'C'
-        if g == consensus_char:
-            consensuspattern= consensuspattern+'G'
+            consensuspattern = consensuspattern + 'A'
+        elif t == consensus_char:
+            consensuspattern = consensuspattern + 'T'
+        elif c == consensus_char:
+            consensuspattern = consensuspattern + 'C'
+        elif g == consensus_char:
+            consensuspattern = consensuspattern + 'G'
 
+    return a_list, t_list, c_list, g_list, consensuspattern
 
-    return a_list,t_list,c_list,g_list,consensuspattern
-
-
+def write_file(a,t,c,g,consensus):
+    with open('Consensusanswer.txt','w') as f:
+        f.write(consensus+"\n")
+        f.write(a+"\n")
+        f.write(c+"\n")
+        f.write(g+"\n")
+        f.write(t+"\n")
+        f.close()
 if __name__ == '__main__':
     file = open('rosalind_cons.txt')
     sequence_list = matrix_builder(file)
-    a,t,c,g,consensus = scoring(sequence_list)
-    print(consensus)
-    print(a)
-    print(t)
-    print(c)
-    print(g)
+    a, t, c, g, consensus = scoring(sequence_list)
+    written = write_file(a, t, c, g, consensus)
