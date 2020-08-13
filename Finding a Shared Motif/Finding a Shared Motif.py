c36c0ab0b46accabb07cace7b8edabe26bb52ea1
HugoCol <hugo.colenbrander@gmail.com>
2020-08-13 09:44:48 +0200

    Consensus and profile alsmost finshed

--- Finding a Shared Motif/Finding a Shared Motif.py
+++ Finding a Shared Motif/Finding a Shared Motif.py
@@ -16,65 +16,32 @@ def file_reader(file):
     print(fasta_list)
     return fasta_list
 
+def common_substring(seq_list):
+    """I did not completely come up with this myself"""
+    longest_substring = ""
 
-def substringsearch(list):
-    """Compare the first two strings and search for matches
-    Every time a match is found the search is extended from that location to
-    find the longest match
-    When that fails to match we start attempting to match the next single
-    character and repeat the extension until all matches are found
-    """
-    right = 1
-    holder = 0
-    matches = []
-    # print(list)
-    for i in range(len(list[0])):
+    full_string = seq_list[0]
+    # The first sequence form the list is taken and sliced in all different
+    # possible lengths and checked to be in all sequences of the list
+    for i in range(len(full_string)):
+        for j in range(i+1, len(full_string)+1):
+            substring = full_string[i:j]
+            # print(i,j)
+            print(substring)
+            in_all = True
+            for seq in seq_list:
+                if substring not in seq:
+                    in_all = False
+                    break
+            # check if the current match is longer than any of the previous
+            # matches
+            if in_all and len(substring) > len(longest_substring):
+                longest_substring = substring
 
-        m = re.search(list[0][(i-holder):right],list[1])
-        if m:
-            # if a match is found with character, attempt to match a longer
-            # string
-            # right extends the length of the re pattern
-            right = right + 1
-            # holder keeps the iteration location in check
-            holder = holder + 1
-            matches.append(m.group(0))
-        else:
-            # if the longer string does not match, reset the length and
-            # start with the next character
-            right=i
-            holder=0
-    print(matches)
-    return matches
-
-def matchpruning(match_list,fasta_list):
-    """In the next part, the found matches are compared to the other strings
-    of all the universal matches, the match == to the longest found match
-    are returned"""
-
-    confirmed_matches = []
-    for i in (match_list):
-
-        for f in fasta_list:
-            # check all potention universal matches
-            m = re.search(i,f)
-        if m:
-            # if they are true, they are added to a list
-            confirmed_matches.append(m.group(0))
-            # the length of the longest match is defined
-    maxlength = max(map(len,confirmed_matches))
-    print(maxlength)
-    # all the matched equal to this length are added to the results
-    result = [longest for longest in confirmed_matches if len(longest) == \
-                                                         maxlength]
-
-    return result
+    # print("Longest common substring:\n", longest_substring)
 
 if __name__ == '__main__':
     file = open("Motiftestdata.txt.txt")
     fasta_list = file_reader(file)
-    match_list = substringsearch(fasta_list)
-
-    result = matchpruning(match_list,fasta_list)
+    common_substring(fasta_list)
     # the first result is printed as it is sufficient to answer the problem
-    print(result[0])
