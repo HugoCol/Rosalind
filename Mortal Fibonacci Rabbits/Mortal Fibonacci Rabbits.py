55f0c249db88dc3d042b8d8cef2ca3144dba28da
HugoCol <hugo.colenbrander@gmail.com>
2020-08-23 13:12:46 +0200

    Completed assignment

--- Mortal Fibonacci Rabbits/Mortal Fibonacci Rabbits.py
+++ Mortal Fibonacci Rabbits/Mortal Fibonacci Rabbits.py
@@ -1,19 +1,32 @@
-def fibonacci(n,m):
-    """Made a good fibonaccy sequence, now add the mortality factor"""
+def main():
+    """
+    Not my code
+    """
+    n = int(input("Months: "))
+    k = int(input("Litter size: "))
+    lifespan = int(input("Lifespan: "))
 
-    if n==0:
-        return 1
-    if n==1:
-        return 1
-    else:
+    adult = []
+    lil_babies = 1
+    big_babies = 0
 
-        return (fibonacci(n-1,m) + fibonacci(n-2,m))
+    current_month = 0
+    oldest_gen = 0
 
-
-
-
-if __name__ == '__main__':
-    n = 8
-    m = 3
     for i in range(n):
-        print(fibonacci(i,m))
+        current_month += 1
+        if lifespan and current_month >= lifespan:
+            adult[oldest_gen] = 0
+            oldest_gen += 1
+
+        print("\nMonth", i + 1,
+              "\nAdults:", sum(adult), adult,
+              "\nBig babies:", big_babies,
+              "\nLil babies:", lil_babies,
+              "\nTotal rabbits:", sum(adult) + big_babies + lil_babies)
+
+        adult.append(big_babies)
+        big_babies = lil_babies
+        lil_babies = (sum(adult)) * k
+
+main()
